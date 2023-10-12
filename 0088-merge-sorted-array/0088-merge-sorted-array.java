8f24c972e0fd8f7116d5881c686695b282b0f506
Anurag Uttam <136011330+ianuraguttam@users.noreply.github.com>
2023-10-12 22:52:57 +0530

    Time: 0 ms (100%), Space: 41.3 MB (35.23%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //variables to work as pointers
+        int i=m-1; // will point at m-1 index of nums1 array
+        int j=n-1; // will point at n-1 index of nums2 array
+        int k=nums1.length-1; //will point at the last position of the nums1 array
+
+        // Now traversing the nums2 array
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                k--; 
+                i--; //updating pointer for further comparisons
+            }else{
+                nums1[k] = nums2[j];
+                k--; 
+                j--; //updating pointer for further comparisons
+            }
+        }
+    }
+}
