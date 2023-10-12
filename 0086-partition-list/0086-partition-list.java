d40e4c92d6fb54f8429ee2d5d7b0896a3df13454
Anurag Uttam <136011330+ianuraguttam@users.noreply.github.com>
2023-10-12 08:17:44 +0530

    Time: 1 ms (8.25%), Space: 41.4 MB (10.18%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head == null || head.next == null)
+            return head;
+        Queue<Integer> smaller = new LinkedList<>();
+        Queue<Integer> greater = new LinkedList<>();
+        ListNode temp = head;
+        while(temp!=null)
+        {
+            if(temp.val < x)
+                smaller.add(temp.val);
+            else
+                greater.add(temp.val);
+            temp = temp.next;
+        }
+        temp = head;
+        while(temp!=null)
+        {
+            if(smaller.size() > 0)
+                temp.val = smaller.poll();
+            else
+                temp.val = greater.poll();
+            temp = temp.next;
+        }
+        return head;
+    }
+}
